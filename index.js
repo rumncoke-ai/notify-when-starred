4d5e2808d9cb232e4582308a82d479f8e6ea98f4
Rumaisa Chowdhury <rumaisa@Umars-MacBook-Air.local>
2023-05-04 16:29:04 -0400

    Final Commit

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-//const axios = require("axios").default;
-import axios from 'axios';
+const axios = require("axios").default;
+//import axios from 'axios';
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
     const body = JSON.parse(event.body);
-    const username = body.sender.login;
-    const avatarUrl = body.sender.avatar_url;
-    const repoName = body.repository.name;
-    const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: `:sparkles: ${username} just starred ${repoName}! :sparkles:`,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
+    if(body.action === "created"){
+      const username = body.sender.login;
+      const avatarUrl = body.sender.avatar_url;
+      const repoName = body.repository.name;
+      const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+        content: `:sparkles: ${username} just starred ${repoName}! :sparkles:`,
+        embeds: [
+          {
+            image: {
+              url: avatarUrl,
+              
+            },
+            
           },
-        },
-      ],
-    });
-    console.log("Submitted!");
-    return {
-      statusCode: 204,
-    };
+          ],
+      });
+      
+      console.log("Submitted!");
+      return { statusCode: 204};
+    }
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
+  
 };
